b2c7058c8aaa05f5df6a5ff61bb96b88cee83b64
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-07-18 22:23:53 -0300

    refactor: remove unused

--- src/main/java/alveradkatsuro/com/br/meu_bolsista/model/audit/Auditable.java
+++ src/main/java/alveradkatsuro/com/br/meu_bolsista/model/audit/Auditable.java
@@ -11,8 +11,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
