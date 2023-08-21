43a7eec542959edc3564d940db2735f2ee6ef203
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-08-21 19:24:42 -0300

    feat: use url image

--- src/main/java/alveradkatsuro/com/br/meu_bolsista/controller/plano_trabalho/PlanoTrabalhoController.java
+++ src/main/java/alveradkatsuro/com/br/meu_bolsista/controller/plano_trabalho/PlanoTrabalhoController.java
@@ -27,6 +27,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import alveradkatsuro.com.br.meu_bolsista.config.oauth2.oidc_user.KeycloakOidcUserInfo;
 import alveradkatsuro.com.br.meu_bolsista.config.oauth2.oidc_user.OidcUserInfoCustom;
+import alveradkatsuro.com.br.meu_bolsista.controller.arquivo.ArquivoController;
 import alveradkatsuro.com.br.meu_bolsista.dto.objetivo.ObjetivoDTO;
 import alveradkatsuro.com.br.meu_bolsista.dto.plano_trabalho.PlanoTrabalhoCreateDTO;
 import alveradkatsuro.com.br.meu_bolsista.dto.plano_trabalho.PlanoTrabalhoDTO;
@@ -39,90 +40,103 @@ import alveradkatsuro.com.br.meu_bolsista.service.plano_trabalho.PlanoTrabalhoSe
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/planoTrabalho")
 public class PlanoTrabalhoController {
 
-    private final ModelMapper mapper;
+	private final ModelMapper mapper;
 
-    private final PlanoTrabalhoService planoTrabalhoService;
+	private final PlanoTrabalhoService planoTrabalhoService;
 
-    @GetMapping
-    @Operation(security = { @SecurityRequirement(name = "Bearer") })
-    public Page<PlanoTrabalhoDTO> findAll(
-            @RequestParam(defaultValue = "0", required = false) Integer page,
-            @RequestParam(defaultValue = "20", required = false) Integer size,
-            @RequestParam(defaultValue = "ASC", required = false) Direction direction) {
-        return planoTrabalhoService.findAll(page, size, direction).map(this::fromModel);
-    }
+	@GetMapping
+	@Operation(security = { @SecurityRequirement(name = "Bearer") })
+	public Page<PlanoTrabalhoDTO> findAll(
+			@RequestParam(defaultValue = "0", required = false) Integer page,
+			@RequestParam(defaultValue = "20", required = false) Integer size,
+			@RequestParam(defaultValue = "ASC", required = false) Direction direction) {
+		return planoTrabalhoService.findAll(page, size, direction).map(this::fromModel);
+	}
 
-    @GetMapping(value = "/{id}")
-    @Operation(security = { @SecurityRequirement(name = "Bearer") })
-    public PlanoTrabalhoDTO findByID(@PathVariable Integer id) {
-        return fromModel(planoTrabalhoService.findById(id));
-    }
+	@GetMapping(value = "/{id}")
+	@Operation(security = { @SecurityRequirement(name = "Bearer") })
+	public PlanoTrabalhoDTO findByID(@PathVariable Integer id) {
+		return fromModel(planoTrabalhoService.findById(id));
+	}
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-    @Operation(security = { @SecurityRequirement(name = "Bearer") })
-    public ResponseEntity<ResponseType> save(
-            @RequestPart("planoTrabalho") PlanoTrabalhoCreateDTO planoTrabalhoCreateDTO,
-            @RequestPart("arquivo") MultipartFile arquivo,
-            JwtAuthenticationToken token) throws IOException {
-        OidcUserInfoCustom oidcUserInfoCustom = new KeycloakOidcUserInfo(token);
+	@PreAuthorize("hasRole('ADMIN')")
+	@PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+	@Operation(security = { @SecurityRequirement(name = "Bearer") })
+	public ResponseEntity<ResponseType> save(
+			@RequestPart("planoTrabalho") PlanoTrabalhoCreateDTO planoTrabalhoCreateDTO,
+			@RequestPart("arquivo") MultipartFile arquivo,
+			JwtAuthenticationToken token) throws IOException {
+		OidcUserInfoCustom oidcUserInfoCustom = new KeycloakOidcUserInfo(token);
 
-        PlanoTrabalhoModel planoTrabalho = planoTrabalhoService.save(oidcUserInfoCustom.getId(),
-                planoTrabalhoCreateDTO, arquivo);
+		PlanoTrabalhoModel planoTrabalho = planoTrabalhoService.save(oidcUserInfoCustom.getId(),
+				planoTrabalhoCreateDTO, arquivo);
 
-        return ResponseEntity.created(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(
-                PlanoTrabalhoController.class).findByID(planoTrabalho.getId()))
-                .toUri()).body(ResponseType.SUCESS_SAVE);
-    }
+		return ResponseEntity.created(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(
+				PlanoTrabalhoController.class).findByID(planoTrabalho.getId()))
+				.toUri()).body(ResponseType.SUCESS_SAVE);
+	}
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-    @Operation(security = { @SecurityRequirement(name = "Bearer") })
-    public ResponseEntity<ResponseType> update(
-            @RequestPart("planoTrabalho") PlanoTrabalhoCreateDTO planoTrabalhoCreateDTO,
-            @RequestPart("arquivo") MultipartFile arquivo)
-            throws NotFoundException, IOException {
+	@PreAuthorize("hasRole('ADMIN')")
+	@PutMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+	@Operation(security = { @SecurityRequirement(name = "Bearer") })
+	public ResponseEntity<ResponseType> update(
+			@RequestPart("planoTrabalho") PlanoTrabalhoCreateDTO planoTrabalhoCreateDTO,
+			@RequestPart(required = false) MultipartFile arquivo)
+			throws NotFoundException, IOException {
 
-        PlanoTrabalhoModel planoTrabalho = planoTrabalhoService.update(planoTrabalhoCreateDTO, arquivo);
+		PlanoTrabalhoModel planoTrabalho = planoTrabalhoService.update(planoTrabalhoCreateDTO, arquivo);
 
-        return ResponseEntity.created(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(
-                PlanoTrabalhoController.class).findByID(planoTrabalho.getId()))
-                .toUri()).body(ResponseType.SUCESS_UPDATE);
-    }
+		return ResponseEntity.created(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(
+				PlanoTrabalhoController.class).findByID(planoTrabalho.getId()))
+				.toUri()).body(ResponseType.SUCESS_UPDATE);
+	}
 
-    @DeleteMapping(value = "/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    @Operation(security = { @SecurityRequirement(name = "Bearer") })
+	@DeleteMapping(value = "/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	@Operation(security = { @SecurityRequirement(name = "Bearer") })
 
-    public void deleteByID(@PathVariable Integer id) {
-        planoTrabalhoService.deleteById(id);
-    }
+	public void deleteByID(@PathVariable Integer id) {
+		planoTrabalhoService.deleteById(id);
+	}
 
-    private PlanoTrabalhoDTO fromModel(PlanoTrabalhoModel planoTrabalhoModel) {
-        return PlanoTrabalhoDTO.builder()
-                .areaTrabalho(planoTrabalhoModel.getAreaTrabalho())
-                .descricao(planoTrabalhoModel.getDescricao())
-                .id(planoTrabalhoModel.getId())
-                .liderNome(planoTrabalhoModel.getLider().getNome())
-                .titulo(planoTrabalhoModel.getTitulo())
-                .objetivos(mapper.map(planoTrabalhoModel.getObjetivos(), new TypeToken<Set<ObjetivoDTO>>() {
-                }.getType()))
-                .recursoMateriais(mapper.map(planoTrabalhoModel.getRecursoMateriais(),
-                        new TypeToken<Set<RecursoMaterialDTO>>() {
-                        }.getType()))
-                .pesquisadores(planoTrabalhoModel.getPesquisadores().stream().map(e -> UsuarioPlanoTrabalhoDTO.builder()
-                        .id(e.getUsuario().getId())
-                        .planoTrabalhoId(e.getPlanoTrabalho().getId())
-                        .cargaHoraria(e.getCargaHoraria())
-                        .build()).collect(java.util.stream.Collectors.toSet()))
-                .build();
-    }
+	private PlanoTrabalhoDTO fromModel(PlanoTrabalhoModel planoTrabalhoModel) {
+		try {
+			return PlanoTrabalhoDTO.builder()
+					.areaTrabalho(planoTrabalhoModel.getAreaTrabalho())
+					.descricao(planoTrabalhoModel.getDescricao())
+					.id(planoTrabalhoModel.getId())
+					.capaUrl(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(
+							ArquivoController.class).recuperarArquivo(planoTrabalhoModel.getCapaResourceId()))
+							.toUri().toString())
+					.liderNome(planoTrabalhoModel.getLider().getNome())
+					.titulo(planoTrabalhoModel.getTitulo())
+					.objetivos(mapper.map(planoTrabalhoModel.getObjetivos(),
+							new TypeToken<Set<ObjetivoDTO>>() {
+							}.getType()))
+					.recursoMateriais(mapper.map(planoTrabalhoModel.getRecursoMateriais(),
+							new TypeToken<Set<RecursoMaterialDTO>>() {
+							}.getType()))
+					.pesquisadores(planoTrabalhoModel.getPesquisadores().stream()
+							.map(e -> UsuarioPlanoTrabalhoDTO.builder()
+									.id(e.getUsuario().getId())
+									.planoTrabalhoId(e.getPlanoTrabalho().getId())
+									.cargaHoraria(e.getCargaHoraria())
+									.build())
+							.collect(java.util.stream.Collectors.toSet()))
+					.build();
+		} catch (IllegalArgumentException | SecurityException | IOException | NotFoundException e) {
+			log.catching(e);
+			return null;
+		}
+	}
 
 }
